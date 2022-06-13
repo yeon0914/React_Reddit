9edeea839ae39b0f8f4150ed381bd6996e6d60d0
chaeyeon <yeonoey0914@gmail.com>
2022-06-13 21:34:23 +0900

    feat:set Navbar Logos with Chakra responsive styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,20 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 
 const Navbar: React.FC = () => {
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex>Logos</Flex>
+      <Flex align="center" gap={2}>
+        <Image src="/images/redditlogo.png" height="30px" />
+        <Image
+          src="/images/reddittext.png"
+          height="15px"
+          display={{ base: "none", md: "unset" }} //chakra는 responsive style support함. bases는 모바일(0em이상), md는 48em 이상. 따라서 md일때 unset을 통해 display none 반대를 적용함.
+        />
+      </Flex>
+      {/* <Directory></Directory>
+      <SearchInput></SearchInput>
+      <RightContent></RightContent> */}
     </Flex>
   );
 };
