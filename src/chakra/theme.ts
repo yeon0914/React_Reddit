2d0b9ec54702f5552c51f62ef000c765b5a00772
chaeyeon <yeonoey0914@gmail.com>
2022-06-13 17:03:37 +0900

    feat:theme init usinig Chakra UI

---
+++ src/chakra/theme.ts
@@ -0,0 +1,27 @@
+// 1. Import the extendTheme function
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#ff3c00",
+    },
+  },
+  fonts: {
+    body: `'Open Sans', sans-serif`,
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
+    }),
+  },
+  components: {
+    //Button
+  },
+});
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type LayoutProps = {};
+
+const Layout: React.FC<LayoutProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default Layout;
